bc60de4ec662d909a7971b903fa56a4a70f9ab50
Raunak Jain <raunakjain1002@gmail.com>
2025-09-12 03:55:20 +0530

    Update MarketAnalysis.tsx

--- src/pages/MarketAnalysis.tsx
+++ src/pages/MarketAnalysis.tsx
@@ -26,7 +26,7 @@ const MarketAnalysis = () => {
   useEffect(() => {
     const fetchMarketData = async () => {
       try {
-        const response = await axios.get('https://api.data.gov.in/resource/9ef84268-d588-465a-a308-a864a43d0070?api-key=579b464db66ec23bdd000001cdd3946e44ce4aad7209ff7b23ac571b&format=json&offset=0&limit=10');
+        const response = await axios.get('https://api.data.gov.in/resource/9ef84268-d588-465a-a308-a864a43d0070?api-key=579b464db66ec23bdd000001bf80db6f209c43a45e82ccb60bc016a4&format=json&offset=0&limit=10');
         setMarketData(response.data.records);
         setLoading(false);
       } catch (err) {
